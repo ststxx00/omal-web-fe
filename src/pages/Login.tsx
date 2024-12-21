d03e1d5c91d962b3e53cae1b6880d6f6e3b2132f
ST <ststxx00@gmail.com>
2024-12-22 00:50:06 +0900

    로그인 화면 UI 정렬 픽스

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,12 +4,13 @@ import { Box, Flex, Heading, Stack, Input, Button } from '@chakra-ui/react';
 const Login: React.FC = () => {
   return (
     <Flex height="100vh">
-      <Box flex="1" display="flex" alignItems="center" justifyContent="center" pl={100}>
+      <Box flex="1" display="flex" alignItems="center" justifyContent="flex-end">
         <Flex direction="column" align="flex-start" pb={20}>
           <Heading as="h1" size="7xl">Welcome,</Heading>
           <Heading as="h1" size="4xl">오늘의 말씀 관리자 페이지입니다.</Heading>
         </Flex>
       </Box>
+    <Box width="10%" />
       <Box flex="1" display="flex" alignItems="center" justifyContent="flex-start">
         <Box p={8} borderWidth={1} borderRadius="lg" boxShadow="lg" width="100%" maxWidth="md" bg="white">
           <Stack gap={4}>
